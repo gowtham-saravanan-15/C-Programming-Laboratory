90c12751251166d60ee641c9b8295a5b094d34d5
gowtham-saravanan-15 <mr.gowtham1510@gmail.com>
2024-09-30 14:52:51 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the numbers you want to add");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("The sum of numbers is:%d",c);
+    return 12;
+}
