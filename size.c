78b8800b3e676076b1f855b55d1f7ff5a0c3053f
gowtham-saravanan-15 <mr.gowtham1510@gmail.com>
2024-09-30 15:27:21 +0530

    Create size.c

---
+++ size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("The size of integer is:%dbytes\n",sizeof(int));
+    printf("The size of character is:%dbytes\n",sizeof(char));
+    printf("The size of double is:%dbytes\n",sizeof(double));
+    printf("The size of float is:%d bytes\n",sizeof(float));
+    return 0;
+}
