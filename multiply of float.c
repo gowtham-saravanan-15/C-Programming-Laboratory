2e2126f141a87f888b104f34b1bc84588a4b2634
gowtham-saravanan-15 <mr.gowtham1510@gmail.com>
2024-09-30 14:54:03 +0530

    Create multiply of float.c

---
+++ multiply of float.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter the numbers you want to multiply");
+    scanf("%f%f",&a,&b);
+    c=a*b;
+    printf("The value of two multiplied numbers is:%f",c);
+    return 0;
+}
