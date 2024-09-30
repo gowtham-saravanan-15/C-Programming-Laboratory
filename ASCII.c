f29b75e76b25d59ef94970231ccb2a312a848c22
gowtham-saravanan-15 <mr.gowtham1510@gmail.com>
2024-09-30 14:58:16 +0530

    Create ASCII.c

---
+++ ASCII.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char as;
+    printf("Enter the character:");
+    scanf("%c",&as);
+    printf("The ASCII value of the character %c is %d",as,as);
+    return 0;
+}
